29a4f7b588c3eccdfe4c5e8fb67eb92a931703cc
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-02-05 14:05:39 +0000

    done

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,38 @@
 public class Review1 {
 
+	public static void main(String[] args) {
+		q1();
+		q2();
+		q3();
+		q4();
+		q5();
+	}
+
 	public static void q1() {
-		//Write question 1 code here
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		int int1 = 9/5;
+		int int2 = 9%5;
+		System.out.println(int1);
+		System.out.println(int2);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Boolean true1 = true;
+		System.out.println(true1);
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		String string1 = "Hello";
+		String string2 = "World";
+		System.out.print(string1);
+		System.out.print(" " + string2);
 	}
 
 }
